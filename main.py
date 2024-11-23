d05e51563a777eb62bcd8b2e0f686c0b5a39d53c
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-23 13:10:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 
    class MyFrame(wx.Frame):
        def __init__(self, parent, title):
-           super().__init__(parent, title=title, size=(300, 200))
-           self.panel = wx.Panel(self)
-           self.button = wx.Button(self.panel, label="Click me")
-           self.button.Bind(wx.EVT_BUTTON, self.on_click)
+           super(MyFrame, self).__init__(parent, title=title)
 
-       def on_click(self, event):
-           wx.MessageBox("Hello from wxPython!", "Message", wx.OK | wx.ICON_INFORMATION)
-
-   if __name__ == '__main__':
-       app = wx.App()
+           self.Show(True)
+           
+class MyApp(wx.App):
+   def OnInit(self):
        frame = MyFrame(None, "My wxPython App")
        frame.Show()
+       return True
+
+   if __name__ == "__main__":
+       app = MyApp()
        app.MainLoop()
