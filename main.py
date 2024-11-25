75057f923ff6bbaa7ea081cf1b4d78916268b48a
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-11-25 14:39:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,28 @@
 import wx
-from utils import ensure_hdpi
-class MyFrame(wx.Frame):
-       def __init__(self, parent, title):
-           super(MyFrame, self).__init__(parent, title=title)
 
-           self.SetSize(self.FromDIP(wx.Size(400, 300)))
-           self.panel = wx.Panel(self)
+app = wx.app()
 
-           self.fileContents = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE)
-           self.button = wx.Button(self.panel, label="Open File")
+icon = wx.Icon("catch!.ico")
 
-           self.text = wxStaticText(self.panel, label="Hello, wxPython!")
+frame = wx.Frame(None, title="Catch!")
+frame.SetIcon(icon)
+frame.SetBackgroundColour("#FF0000")
+frame.SetSize((400, 400))
+panel = wx.Panel(frame)
 
+current_font = panel.GetFont()
+current_font.SetPointSize(16)
+current_font.SetUnderlined(True)
+current_font.SetWeight(wx.FONTWEIGHT_BOLD)
+current_font
+panel.SetFont(current_font)
 
-           self.Bind(wx.EVT_BUTTON, self.on_click, self.button)
+text = wx.StaticText(panel, label="Hello World", size=(100, 100), pos=(100, 100))
+frameSetBackgroundColour("#ffce00")
+frameSetForegroundColour("#ff4d00")
 
-           self.sizer = wx.BoxSizer(wx.VERTICAL)
-           self.sizer.Add(self.fileContents, 1, wx.EXPAND)
-           self.sizer.Add(self.button, 0, wx.ALIGN_CENTER | wx.ALL, self.FromDIP(10))
-           self.sizer.AddStretchSpacer()
+image = wx.StaticBitmap(panel)
 
-           self.panel.SetSizer(self.sizer)
-
-           self.Show(True)
-           
-        def OnButtonClick(self, event):
-            dialog = wx.FileDialog(self, "Open File", style=wx.FD_OPEN | wx.FILE_MUST_EXIST
-                                   wildcard= "Video files (*.mp4)|*.mp4")
-            if dialog.ShowModal() == wx.ID_OK:
-               wx.MessageBox("Success", "Info", wx.OK)    
-                try:
-                    with open(dialog.GetPath()), 'r') as file:
-                        self.fileContents.SetValue(file.read())
-            except Exception as e:
-                wx.MessageBox(str(e), "Error", wx.OK | wx.ICON_ERROR)
-            
-class MyApp(wx.App):
-   def OnInit(self):
-       
-       ensure_hdpi()
-
-       frame = MyFrame(None, "My wxPython App")
-       frame.Show()
-       return True
-
-   if __name__ == "__main__":
-       app = MyApp()
-       app.MainLoop()
+frame.Center()
+frame.Show()
+app.MainLoop()
