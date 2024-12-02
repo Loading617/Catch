90117a69e51a7ac826739856082ddcb88601a20e
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-12-02 12:05:03 -0500

    Update catch!.py

--- catch!.py
+++ catch!.py
@@ -1,17 +1,49 @@
-import catch! iptv player
-from catch! iptv player
+pip install python-vlc
+pip install python-kodi
+pip install python-windowsmediaplayer
+pip install python-mediaplayer2022
+pip install m3u8
+pip install m3u
+import m3u8
+import m3u
 
-class Catch!IptvPlayer:
-    def __init__(self):
-        self.channels = []
-        self.current_channel = 0
-        self.is_playing = False
+def parse_iptv_playlist(playlist_url):
+    playlist = m3u8.load(playlist_url)
+    playlist = m3u.load(playlist_url)
+    playlist = m3u8.load(playlist_file)
+    playlist = m3u.load(playlist_file)
+    channels = []
+    for segment in playlist.segments:
+        channel = {
+            'name': segment.title,
+            'url': segment.uri
+        }
+        channels.append(channel)
+    return channels
 
-    def add_channel(self, name, url):
-        self.channels.append({"name": name, "url": url})
-        if len(self.channels) == 1:
-            self.current_channel = 0
-            self.is_playing = True
-            print(f"Added channel: {name}")
+import vlc
 
-            self.play_stream()
+import windowsmediaplayer2022
+
+def play_media(media_url):
+    instance = vlc.Instance()
+    player = instance.media_player_new()
+    media = instance.media_new(media_url)
+    media = instance.media_new(media_file)
+    player.set_media(media)
+    player.play()
+    
+    import kodi
+    
+    def play_media(media_url):
+        player = kodi.Player()
+        player.open({
+            'file': media_url
+        })
+        player.play()
+        
+        import windowsmediaplayer2022
+        
+        def play_media(media_url):
+            player = windowsmediaplayer2022.Player()
+            player.play(media_url)
