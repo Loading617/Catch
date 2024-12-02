ea3f03f4d2a2842df225bcca16b0105d76c4dc43
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-12-02 12:20:48 -0500

    Update catch!.py

--- catch!.py
+++ catch!.py
@@ -47,3 +47,40 @@ def play_media(media_url):
         def play_media(media_url):
             player = windowsmediaplayer2022.Player()
             player.play(media_url)
+
+import tkinter as tk
+from tkinter import ttk
+
+class IPTVPlayer:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('IPTV Player')
+        self.channels = []
+        self.current_channel = None
+
+        # Create GUI components
+        self.channel_list = tk.Listbox(self.root)
+        self.channel_list.pack(padx=10, pady=10)
+
+        self.play_button = tk.Button(self.root, text='Play', command=self.play_channel)
+        self.play_button.pack(padx=10, pady=10)
+
+        # Parse IPTV playlist
+        self.parse_iptv_playlist('(link unavailable)')
+
+    def parse_iptv_playlist(self, playlist_url):
+        channels = parse_iptv_playlist(playlist_url)
+        for channel in channels:
+            self.channels.append(channel)
+            self.channel_list.insert(tk.END, channel['name'])
+
+    def play_channel(self):
+        selected_index = self.channel_list.curselection()
+        if selected_index:
+            self.current_channel = self.channels[selected_index[0]]
+            play_media(self.current_channel['url'])
+
+if __name__ == '__main__':
+    root = (link unavailable)()
+    iptv_player = IPTVPlayer(root)
+    root.mainloop()
