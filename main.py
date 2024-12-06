48b647affc37ba001ff315a1d8207eb1f6a89df7
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-06 11:27:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-import wx
-from utils import ensure_hdpi
-
-app = wx.app()
-
-icon = wx.Icon("catch!.ico")
-
-frame = wx.Frame(None, title="Catch!")
-frame.SetIcon(icon)
-frame.SetBackgroundColour("#FF0000")
-frame.SetSize((400, 400))
-panel = wx.Panel(frame)
-
-current_font = panel.GetFont()
-current_font.SetPointSize(16)
-current_font.SetWeight(wx.FONTWEIGHT_BOLD)
-current_font
-panel.SetFont(current_font)
-
-application_bitmap = wx.ApplicationBitmap("Catch!.ico")
-image = wx.StaticBitmap(panel, bitmap=application_bitmap, pos=(150,0))
-
-frame.Center()
-frame.Show()
-app.MainLoop()
+from tkinter import *
+Window = Tk()
+Window.mainloop()
