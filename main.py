621d60c6ef6041efafc7a33290030f9add5ccc2c
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-06 13:26:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 root = tk.Tk()
 
-root.geometry("800x500")
+root.geometry("500x500")
 root.title("Catch!")
 
 label = tk.Label(root, text="Catch!", font=("Helvetica, 18"))
@@ -10,5 +10,8 @@ label.pack(padx=20, pady=20)
 
 textbox = tk.Text(root, height=4, font=("Helvetica, 14"))
 textbox.pack()
+
+myentry = tk.Entry(root)
+myentry.pack()
 
 root.mainloop()
