6982843251df3239b57ae73a76f2b6451a3866cd
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-06 12:58:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ root.title("Catch!")
 label = tk.Label(root, text="Catch!", font=("Helvetica, 18"))
 label.pack(padx=20, pady=20)
 
+textbox = tk.Text(root, font=("Helvetica, 14"))
+textbox.pack()
+
 root.mainloop()
